46f98c54de5a75db1f5c96e89843f92dda7f4f56
Hikari-chan <yuukishiroko@gmail.com>
2017-01-30 11:12:47 -0500

    Doubt this will fix anything, but...

--- source/main.c
+++ source/main.c
@@ -7,11 +7,11 @@ void finished() {
 }
 
 int main(int argc, char **argv) {
-    gfxInitDefault();
-    consoleInit(GFX_TOP, NULL);
-    printf("ClearPendingTitles\nby Hikari\n\n");
-    printf("Initializing AM...\n");
-    amInit();
+	gfxInitDefault();
+	consoleInit(GFX_TOP, NULL);
+	printf("ClearPendingTitles\nby Hikari\n\n");
+	printf("Initializing AM...\n");
+	amInit();
 		printf("Press A to clear pending NAND title installs.\n");
 		printf("Press X to clear pending SD title installs.\n");
 		printf("Press Y to clear all pending title installs.\n");
@@ -19,34 +19,32 @@ int main(int argc, char **argv) {
 			while (aptMainLoop()) {
 				hidScanInput();
 				u32 kDown = hidKeysDown();
-
-					if (kDown & KEY_A) {
-						printf("Clearing pending NAND title installs...\n");
-						AM_DeleteAllPendingTitles(MEDIATYPE_NAND);
-						finished();
-					}
-					else if (kDown & KEY_X) {
-						printf("Clearing pending SD title installs...\n");
-						AM_DeleteAllPendingTitles(MEDIATYPE_SD);
-						finished();
-					}
-				 	else if (kDown & KEY_Y) {
-						printf("Clearing all pending title installs...\n");
-						AM_DeleteAllPendingTitles(MEDIATYPE_NAND);
-						AM_DeleteAllPendingTitles(MEDIATYPE_SD);
-						finished();
-					}
-					else if (kDown & KEY_B) {
-						printf("Exiting...\n");
-						break;
-					}
-			}
-
-		amExit();
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-		gspWaitForVBlank();
-
-    gfxExit();
-    return 0;
+				if (kDown & KEY_A) {
+					printf("Clearing pending NAND title installs...\n");
+					AM_DeleteAllPendingTitles(MEDIATYPE_NAND);
+					finished();
+				}
+				else if (kDown & KEY_X) {
+					printf("Clearing pending SD title installs...\n");
+					AM_DeleteAllPendingTitles(MEDIATYPE_SD);
+					finished();
+				}
+				else if (kDown & KEY_Y) {
+					printf("Clearing all pending title installs...\n");
+					AM_DeleteAllPendingTitles(MEDIATYPE_SD);
+					svcSleepThread(100);
+					AM_DeleteAllPendingTitles(MEDIATYPE_NAND);
+					finished();
+				}
+				else if (kDown & KEY_B) {
+					printf("Exiting...\n");
+					break;
+				}
+		}
+	amExit();
+	gfxFlushBuffers();
+	gfxSwapBuffers();
+	gspWaitForVBlank();
+	gfxExit();
+	return 0;
 }
